1c5816c8f2fc6f940c3dafb19065be710055a640
Kishan Kumar Gupta <kkgupta0106@gmail.com>
2025-06-17 14:36:42 +0530

    update

--- 01_Basics/second.java
+++ 01_Basics/second.java
@@ -2,6 +2,9 @@ import java.lang.*;
 
 public class second {
     public static void main(String[] args) {
-        System.out.println("Hello, World! 2");
+        System.out.println("Hello, World!");
+        System.out.println("First Program");
+        System.out.println("Second Program");
     }
 }
+
