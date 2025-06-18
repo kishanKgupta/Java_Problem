bee89b6a2992ad55d17949c73c7282132d391074
Kishan Kumar Gupta <kkgupta0106@gmail.com>
2025-06-18 09:40:48 +0530

    add new questions

---
+++ 01_Basics/Third.java
@@ -0,0 +1,7 @@
+import java.lang.*;
+
+public class Third {
+    public static void main(String[] args){
+        System.out.println("Start Java Programming!");
+    }
+}
---
+++ 02_PatternQuestions/rightAngle.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+
+public class rightAngle {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
